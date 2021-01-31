ecd33b23de7a8b9bde4a793e883d7841ad2b4db4
David Fraser <47202564+DavidMatthewFraser@users.noreply.github.com>
2021-01-31 12:25:32 -0700

    initial commit

---
+++ league_coach.py
@@ -0,0 +1,26 @@
+import pyttsx3 as pp
+import time as t
+import random 
+
+engine = pp.init()
+
+name = 'David'
+
+interval = 3
+minutes = 60 * interval
+
+lines = [
+    'where is the enemy jungler',
+    'are there any lanes to gank',
+    'who is strong on their team and who is weak',
+    'who should you target in team fights',
+    'what objectives are up soon',
+    'who has flash and who does not',
+    'are you buying wards',
+    'have more fun, your not having enough fun'
+]
+
+while True:
+    t.sleep(minutes)
+    engine.say("hey " + name + ", " + lines[random.randint(0, len(lines)-1)])
+    engine.runAndWait()
